3a523f99e5ff6803b6fb1e68cedf98f453d62cca
syedz226 <zilanahamed@gmail.com>
2025-08-12 13:40:00 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,8 +1,22 @@
-def gitDownload(repo)
+def GD(repo)
 {
   git "https://github.com/IntelliqDevops/${repo}.git"
 }
-def mavenBuild()
+def MB()
 {
   sh 'mvn package'
 }
+def CDeploy(ip)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/testapp1.war'
+}
+def CT(ft)
+{
+  git 'https://github.com/IntelliqDevops/${ft}.git'
+  sh 'java -jar /home/ubuntu/.jenkins/workspace/DP1SCP/testing.jar'
+}
+def CDelivery(ip)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/testapp1.war'
+}
+  
