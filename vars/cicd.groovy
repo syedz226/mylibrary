9b79231dcd4256ff067f1cc7f6fe6469e6cdd813
syedz226 <zilanahamed@gmail.com>
2025-08-12 13:59:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,17 +6,17 @@ def MB()
 {
   sh 'mvn package'
 }
-def CDeploy(ip)
+def CDeploy(ip,testapp)
 {
-  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/testapp1.war'
+  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${testapp}.war'
 }
 def CT(ft)
 {
   git "https://github.com/IntelliqDevops/${ft}.git"
   sh 'java -jar /home/ubuntu/.jenkins/workspace/DP1SCP/testing.jar'
 }
-def CDelivery(ip)
+def CDelivery(ip,prodapp)
 {
-  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/testapp1.war'
+  sh 'scp /home/ubuntu/.jenkins/workspace/DP1SCP/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${prodapp}.war'
 }
   
